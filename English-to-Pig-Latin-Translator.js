1c555c7015f5226ef9f4be1ae11f72b07f49805f
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-06-06 21:51:48 +0200

    Correction and final version of the game

--- English-to-Pig-Latin-Translator.js
+++ English-to-Pig-Latin-Translator.js
@@ -1,19 +1,25 @@
+// Creating an new array containing only the players input and transforming each element inside the array to lower case
 const playerInput = process.argv.slice(2).map(word => word.toLowerCase());
 
 const translatedArray = [];
 
+// Function checking if either the first or second letter of an element in array contains a vowel.
+// After checking the function transforms the Word acordingly and push's it into an new Array.
 function convertToPigLatin(word) {
     if (['a','e','i','o','u'].includes(word.charAt(0))) {
         translatedArray.push(word + 'way');
     } else if (['a','e','i','o','u'].includes(word.charAt(1))) {
         translatedArray.push(word.slice(1) + word.charAt(0) + 'ay');
     } else {
-        translatedArray.push(word.slice(2) + word.charAt(0) + word.charAt(1) + 'ay');
+        translatedArray.push(word.slice(2) + word.charAt(0,1) + word.charAt(1) + 'ay');
     }
 }
 
+// Calling the function on each word (element) of the Players Input Array
 playerInput.forEach(word => {
     convertToPigLatin(word);
 });
 
-console.log(translatedArray);
+// Loggin the original and the translated phrase into the console
+console.log(`English Phrase: ${playerInput.join(' ')}`);
+console.log(`Pig Latin Translation: ${translatedArray.join(' ')}`);
